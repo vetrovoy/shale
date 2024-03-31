d99d3c09a47908d285ffb9c31058e85901760f18
v3trovoy <v3trovoy@gmail.com>
2024-03-31 17:36:40 +0300

    Initial project commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,28 @@
-import '../css/app.css';
+import "../css/app.css";
 
-console.log(1);
+
+const burger = document.querySelector("#hamburger");
+
+burger.addEventListener("click", () => {
+  burger.classList.toggle("active");
+  // document.querySelector(".menu").classList.toggle("menu--active");
+})
+
+const workSlider = new Swiper(".work-slider .swiper", {
+  rewind: true,
+  spaceBetween: 0,
+
+  slidesPerView: 1,
+  grabCursor: true,
+
+  breakpoints: {
+    990: {
+      spaceBetween: 20,
+      slidesPerView: 2,
+    },
+    1200: {
+      slidesPerView: "auto",
+      spaceBetween: 37,
+    },
+  },
+});
